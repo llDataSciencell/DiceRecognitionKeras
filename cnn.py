864df93eeb4da82ba2367807fd4f69746057e010
llDataSciencell <b6800668@planet.kanazawa-it.ac.jp>
2020-02-11 22:13:12 +0900

    test_generatorのtest_paramsに関するバグを修正。最適化関数のAdamの学習率を0.0005など下げることにより改善。

--- cnn.py
+++ cnn.py
@@ -3,33 +3,31 @@
 import math
 import pandas as pd
 from keras.models import Model, Sequential,load_model
+from keras.optimizers import Adam
 from keras.layers import Conv2D, Dense, Dropout, Flatten
-import data_generator
+import data_generator # DataGeneratorFactory クラスが記述されているファイルをimport
 import os
 from keras.callbacks import EarlyStopping,ModelCheckpoint
-NUM_EPOCHS = 1
+NUM_EPOCHS = 20 #TODO エポック数を必要に応じて増やす
 BATCH_SIZE = 16
 INPUT_SHAPE=(128,128)
 
 def get_model():
   model = Sequential()
-  model.add(Conv2D(24, (3, 3), activation='relu',
+  model.add(Conv2D(12, (3, 3), activation='relu',
                    input_shape=(128, 128, 1)))
-  model.add(Conv2D(36, (3, 3), activation='relu'))
+  model.add(Conv2D(24, (3, 3), activation='relu'))
   model.add(Conv2D(48, (3, 3), activation='relu'))
   model.add(Conv2D(64, (3, 3), activation='relu'))
   model.add(Conv2D(64, (3, 3), activation='relu'))
-  model.add(Conv2D(64, (3, 3), activation='relu'))
   model.add(Flatten())
-  model.add(Dropout(0.5))
-  model.add(Dense(128, activation='relu'))
-  model.add(Dense(64, activation='relu'))
+  model.add(Dense(256, activation='relu'))
   model.add(Dense(6, activation='sigmoid'))
   return model
 
 def train():
   model = get_model()
-  model.compile(loss='categorical_crossentropy', optimizer='adam',
+  model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0005, beta_1=0.9, beta_2=0.999),
                 metrics=['accuracy'])
 
   data_directory=os.environ['HOME']+"/DATA/NicoTechDice/DiceDataset/"
@@ -42,7 +40,7 @@ def train():
   print("valid_size:"+str(dgf.valid_size))
 
   early_stopping_callback = EarlyStopping(monitor='val_loss', patience=10)
-  checkpoint_callback = ModelCheckpoint('../models/model_cnn_temp.h5', monitor='val_loss', verbose=1, save_best_only=True,
+  checkpoint_callback = ModelCheckpoint('../models/model_cnn_temp_20200211.h5', monitor='val_loss', verbose=1, save_best_only=True,
                                         mode='min')
   
   history = model.fit_generator(
@@ -50,22 +48,25 @@ def train():
       steps_per_epoch=math.ceil(dgf.train_size / BATCH_SIZE),
       validation_data=dgf.valid_generator,
       validation_steps=math.ceil(dgf.valid_size / BATCH_SIZE),
-      epochs=NUM_EPOCHS, verbose=1,callbacks=[early_stopping_callback,checkpoint_callback])
+      epochs=NUM_EPOCHS, verbose=1,callbacks=[early_stopping_callback])#,checkpoint_callback])
+  
+  print(history)
 
-  model = load_model('../models/model_cnn_dice.h5')
+  model.save('../models/model_cnn_dice_20200211.h5')
+  #model = load_model('../models/model_cnn_dice.h5')
   metrics = model.evaluate_generator(dgf.test_generator, steps=dgf.test_size)
-
+  print("Evaluate Accuracy for test dataset...")
   for i in range(len(model.metrics_names)):
     metric_name = model.metrics_names[i]
     metric_value = metrics[i]
     print('%s: %s' % (metric_name, metric_value))
+  print("=================================")
 
-  model.save('../models/model_cnn_dice.h5')
-
-  #https://keras.io/models/model/
+  #RGB画像を直接モデルに与えて推論を行い、評価する  (https://keras.io/models/model/)
+  """
   for i in range(100):
-    result=model.predict_generator(dgf.pred_gen(),steps=1)
+    result=model.predict_generator(dgf.pred_gen_using_image(image),steps=1)
     print("result:"+str(result))
-
+  """
 if __name__ == '__main__':
   train()
--- data_generator.py
+++ data_generator.py
@@ -20,13 +20,16 @@ class DataGeneratorFactory:
         width_shift_range=0.2,
         height_shift_range=0.2,
         zoom_range=0.2,
-        horizontal_flip=True)
+        horizontal_flip=True,
+        zca_whitening=True)
     valid_datagen = ImageDataGenerator(
         samplewise_center=True,
-        samplewise_std_normalization=True)
+        samplewise_std_normalization=True,
+        zca_whitening=True)
     self.test_datagen = ImageDataGenerator(
         samplewise_center=True,
-        samplewise_std_normalization=True)
+        samplewise_std_normalization=True,
+        zca_whitening=True)
 
     params = {
         'target_size': self.input_shape,
@@ -45,24 +48,30 @@ class DataGeneratorFactory:
     # setting for test data
     #self.params['batch_size'] = 1
     #self.params['shuffle'] = False
-    self.new_params = {
+    self.test_params = {
         'batch_size': 1,
-        'shuffle': False
+        'shuffle': False,
+        'target_size': self.input_shape,
+        'classes': ['1', '2', '3', '4', '5', '6'],
+        'class_mode': 'categorical',
+        'seed': 1234,
     }
     if self.num_channels == 1:
-      params['color_mode'] = 'grayscale'
+      self.test_params['color_mode'] = 'grayscale'
 
     self.test_generator = self.test_datagen.flow_from_directory(
-        '%s/test' % self.data_dir, **self.new_params)
+        '%s/test' % self.data_dir, **self.test_params)
+
     self.train_size = len(self.train_generator.filenames)
     self.valid_size = len(self.train_generator.filenames)
     self.test_size = len(self.test_generator.filenames)
 
-  def pred_gen(self,image):
+  def pred_gen_using_raw_image(self,image):
       #TODO ここでカメラからの入力画像を受け付ける　imreadの部分
       #image = cv2.imread(os.environ['HOME']+"/DATA/NicoTechDice/DiceDataset/train/1/00239.png",cv2.IMREAD_GRAYSCALE)
       image = cv2.resize(image,(128, 128))
       print(image)
       print(image.shape)
       image=np.reshape(image,(1,image.shape[0],image.shape[1],1))
-      return self.test_datagen.flow(image,**self.new_params)
+      return self.test_datagen.flow(image,**self.test_params)
+  
